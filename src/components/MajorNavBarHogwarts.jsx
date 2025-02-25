8387166ba91e8a5a197187fac1e559d2b2a83e20
Sebastián Betancur Castillo <sebastianbetancurth1306@gmail.com>
2025-02-24 22:07:29 -0500

    En el NavBar se pintan los nombres, colores y emblemas de las casas llamándolos a la base de datos

--- src/components/MajorNavBarHogwarts.jsx
+++ src/components/MajorNavBarHogwarts.jsx
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
 import "../styles/majorNavBarHogwarts.css";
 import { NavLink, useLocation } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
 import { houses } from "../data/houses";
 
 
 export const MajorNavBarHogwarts = () => {
     const location = useLocation();
     const [hoveredHouse, setHoveredHouse] = useState(null);
+
+    const { data: houses, loading, error } = useFetch("http://localhost:5000/api/houses");
+
+    if (loading) return <p>Cargando casas...</p>;
+    if (error) return <p>Error: {error}</p>;
 
     return (
         <>
@@ -28,20 +34,20 @@ export const MajorNavBarHogwarts = () => {
                         <ul className="navbar-nav mx-auto">
                             <li className="nav-item">
                                 <NavLink className="nav-link" to="/">
-                                <img className="ico-logo-links" src="src\assets\images\images-hogwarts\hogwarts-logo-ico.ico" />
-                                <span>Inicio</span> </NavLink>
+                                    <img className="ico-logo-links" src="src\assets\images\images-hogwarts\hogwarts-logo-ico.ico" />
+                                    <span>Inicio</span> </NavLink>
                             </li>
 
                             <li className="nav-item dropdown">
                                 <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    
-                                <img className="ico-link-witch" src="src\assets\images\images-hogwarts\crystal-ball-x32.png" />
-                                   Mundo Mágico
+
+                                    <img className="ico-link-witch" src="src\assets\images\images-hogwarts\crystal-ball-x32.png" />
+                                    Mundo Mágico
 
                                 </a>
                                 <ul class="dropdown-menu">
                                     <li><a className="dropdown-item" href="#">Historia de Hogwarts</a></li>
-                                    <li><hr className="dropdown-divider"/></li>
+                                    <li><hr className="dropdown-divider" /></li>
                                     <li><a className="dropdown-item" href="#">Mapa del Merodeador</a></li>
                                 </ul>
                             </li>
@@ -63,10 +69,10 @@ export const MajorNavBarHogwarts = () => {
                                                     <NavLink
                                                         className="dropdown-item"
                                                         to={`/${house.name}`}
-                                                        onMouseEnter={() => setHoveredHouse(house.id)}
+                                                        onMouseEnter={() => setHoveredHouse(house._id)}
                                                         onMouseLeave={() => setHoveredHouse(null)}
                                                         style={{
-                                                            color: isActive || hoveredHouse === house.id ? house.colors[0] : "black",
+                                                            color: isActive || hoveredHouse === house._id ? house.colors[0] : "black",
                                                             transition: "color 0.3s ease-in-out",
                                                             position: "relative",
                                                             display: "flex",
@@ -74,7 +80,7 @@ export const MajorNavBarHogwarts = () => {
                                                         }}
                                                     >
                                                         <img
-                                                            src={(isActive || hoveredHouse === house.id) ? house.hoverLogo : house.logo}
+                                                            src={(isActive || hoveredHouse === house._id) ? house.emblemHover : house.emblem}
                                                             alt={`${house.name} logo`}
                                                             style={{ width: "40px", marginRight: "10px", transition: "0.3s" }}
                                                         />
@@ -95,7 +101,7 @@ export const MajorNavBarHogwarts = () => {
                                                                 left: "0",
                                                                 width: "100%",
                                                                 height: "10px",
-                                                                transform: isActive || hoveredHouse === house.id ? "scaleX(1)" : "scaleX(0)",
+                                                                transform: isActive || hoveredHouse === house._id ? "scaleX(1)" : "scaleX(0)",
                                                                 transformOrigin: "left",
                                                                 transition: "transform 0.7s ease-in-out",
                                                             }}
@@ -111,32 +117,32 @@ export const MajorNavBarHogwarts = () => {
 
                             <li className="nav-item dropdown">
                                 <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                <img className="ico-link-witch" src="src\assets\images\images-hogwarts\dobby-x32.png" />
-                                    <span>Magia y Personajes</span> 
+                                    <img className="ico-link-witch" src="src\assets\images\images-hogwarts\dobby-x32.png" />
+                                    <span>Magia y Personajes</span>
                                 </a>
                                 <ul class="dropdown-menu">
                                     <li><a className="dropdown-item" href="#">Magos y Brujas</a></li>
-                                    <li><hr className="dropdown-divider"/></li>
+                                    <li><hr className="dropdown-divider" /></li>
                                     <li><a className="dropdown-item" href="#">Criaturas Mágicas</a></li>
                                 </ul>
                             </li>
 
                             <li className="nav-item dropdown">
                                 <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                <img className="ico-links" src="src\assets\images\images-hogwarts\potion-x32.png" />
-                                   <span>Hechicería</span> 
+                                    <img className="ico-links" src="src\assets\images\images-hogwarts\potion-x32.png" />
+                                    <span>Hechicería</span>
                                 </a>
                                 <ul className="dropdown-menu">
                                     <li><a className="dropdown-item" href="#">Hechizos y Pociones</a></li>
-                                    <li><hr className="dropdown-divider"/></li>
+                                    <li><hr className="dropdown-divider" /></li>
                                     <li><a className="dropdown-item" href="#">Tienda Mágica</a></li>
                                 </ul>
                             </li>
 
                             <li className="nav-item">
                                 <NavLink className="nav-link" to="/what-is-your-house">
-                                <img className="ico-links" src="src\assets\images\images-hogwarts\hat-selector-x32.png" />
-                                 <span>Sombrero Seleccionador</span></NavLink>
+                                    <img className="ico-links" src="src\assets\images\images-hogwarts\hat-selector-x32.png" />
+                                    <span>Sombrero Seleccionador</span></NavLink>
                             </li>
 
                         </ul>
---
+++ src/data/home.js
@@ -0,0 +1,12 @@
+import hogwartsLogo from "../assets/images/images-hogwarts/hogwarts2.jpg";
+
+
+export const home = [
+    {
+      id: 1,
+      name: "Home",
+      colors: ["#DA5A2A", "#D3A625"],
+      frontpage: hogwartsLogo,
+    },
+    
+  ];
