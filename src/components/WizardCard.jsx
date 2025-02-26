6d5d19b02364a278f6e1266681f7bd8748345d56
Sebastián Betancur Castillo <sebastianbetancurth1306@gmail.com>
2025-02-26 15:45:59 -0500

    Se está implementando la tarjeta para los magos

--- src/components/WizardCard.jsx
+++ src/components/WizardCard.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import "../styles/wizardCard.css";
 
 export const WizardCard = () => {
-  return (
-    <div>WizardCard</div>
-  )
+    return (
+        <>
+            <div className='d-flex justify-content-center flex-wrap p-3'>
+                <div className="flip-card">
+                    <div className="flip-card-inner">
+                        <div className="flip-card-front p-2">
+
+                            <div style={{ backgroundColor: 'blue', height: 400, }}>
+                                <p>Imagen</p>
+                            </div>
+
+
+
+                        </div>
+                        <div className="flip-card-back">
+                            <h1>John Doe</h1>
+                            <p>Architect & Engineer</p>
+                            <p>We love that guy</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </>
+
+    )
 }
 
 
